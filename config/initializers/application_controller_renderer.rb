f98fac256380a7506db5c85ccb4c821ccfd44e36
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-18 14:01:05 +0000

    [14]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6275a0c44561103f58c720455f0862486fc4421e457d46c26d2cf1e86c6bece5f41d7a456a33a1ec5a4fa5c65b03d09c356c221567efc665f567b386545aac9b'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'users/show'
   root to: 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+  resources :users, only: [:show]
+ 
 end
